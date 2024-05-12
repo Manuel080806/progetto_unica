1578bf60a8b960adb5ad01e49e2013d9096d11c3
Manuel080806 <146246797+Manuel080806@users.noreply.github.com>
2024-05-12 16:15:30 +0200

    Delete python directory

--- python/es/05.py
+++
@@ -1,6 +0,0 @@
-testo = input("Inserisci un testo: ")
-
-if testo == testo[::-1]:
-    print("Il testo Ã¨ palindromo")
-else:
-    print("Il testo non Ã¨ palindromo")
--- python/ordinamento/01.py
+++
@@ -1,7 +0,0 @@
-lista=[8,5,2,6,9,3,1,4,0,7]
-for i in range(len(lista)-1):
-    for j in range(i+1,len(lista)):
-        if(lista[i] > lista [j]):
-            lista[i], lista[j] = lista[j], lista[i]
-
-print(lista)
